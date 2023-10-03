f3e98cd0c74d2d9c25e2ea5281db0e3c720d0874
ntruter42 <ntruter42@gmail.com>
2023-10-03 22:47:20 +0200

    Added backend functionality for filtering by size

--- routes/catalog_api.js
+++ routes/catalog_api.js
@@ -4,6 +4,8 @@ import { services } from "../index.js";
 const shoes_api = Router();
 const carts_api = Router();
 
+// ========== SHOES API ROUTES ========== //
+
 shoes_api.get('/', async (req, res) => {
 	res.status(200).send(await services.getShoes());
 });
@@ -12,6 +14,13 @@ shoes_api.get('/brand/:brandname', async (req, res) => {
 	const filters = { brand: req.params.brandname };
 	res.status(200).send(await services.getShoes(filters));
 });
+
+shoes_api.get('/size/:size', async (req, res) => {
+	const filters = { size: req.params.size };
+	res.status(200).send(await services.getShoes(filters));
+});
+
+// ========== CARTS API ROUTES ========== //
 
 carts_api.get('/', async (req, res) => {
 	const user_id = 100; // get user_id for logged in user ie. req.session.user_id
