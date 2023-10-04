3d50c2ef57b4f8ed0d493d69de2854e66d7ce1be
ntruter42 <ntruter42@gmail.com>
2023-10-04 10:47:21 +0200

    Replaced frontend for shoe display with 404 error

--- routes/catalog_routes.js
+++ routes/catalog_routes.js
@@ -1,21 +1,9 @@
 import { Router } from "express";
-import { services } from "../index.js";
-import axios from "axios";
 
 const router = Router();
 
-router.get('/', async (req, res) => {
-	try {
-		const host = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
-		const shoes = (await axios.get(`${host}api/shoes`)).data;
-
-		res.render('index', {
-			title: "Shoe Catalog",
-			shoes
-		});
-	} catch (error) {
-		res.status(500).send('Error');
-	}
+router.get('/', (req, res) => {
+	res.render('index');
 });
 
 export default router;
